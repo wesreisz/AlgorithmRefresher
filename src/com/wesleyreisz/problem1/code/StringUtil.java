1ff02a417c360038f44138ee31f83ad1e1b9eb9f
Wesley Reisz <wes@wesleyreisz.com>
2014-09-13 00:35:40 -0400

    practice

--- src/com/wesleyreisz/problem1/code/StringUtil.java
+++ src/com/wesleyreisz/problem1/code/StringUtil.java
@@ -1,6 +1,9 @@
 package com.wesleyreisz.problem1.code;
 
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class StringUtil {
     /**
      * 1.1 Implement an algorithm to determine if a string has all unique characters. What if you can not use additional
@@ -28,5 +31,81 @@ public class StringUtil {
             result+=String.valueOf(input.charAt(i));
         }
         return result;
+    }
+    /**
+     * 1.3 Design an algorithm and write code to remove the duplicate characters in a string without using any additional buff
+     */
+    public static String removeDuplicateCharsInitial(String input){
+        Set results = new HashSet();
+        for(int i=0;i<input.length();i++){
+            results.add(Character.valueOf(input.charAt(i)));
+        }
+        return results.toString();
+    }
+
+    public static void removeDuplicatesSolution(String input) {{
+        char[] str = input.toCharArray();
+        if (str == null) return;
+        int len = str.length;
+        if (len < 2) return;
+
+        int tail = 1;
+
+        for (int i = 1; i < len; ++i) {
+            int j;
+            for (j = 0; j < tail; ++j) {
+                if (str[i] == str[j]) break;
+            }
+            if (j == tail) {
+                str[tail] = str[i];
+                ++tail;
+            }
+        }
+        str[tail] = 0;
+    }
+    }
+
+
+    /**
+     * <My solution... it run O(n2)
+     * @param set1
+     * @return
+     */
+    public static String removeDuplicatesSolutionNew(String set1) {
+        Set<Character> results = new HashSet<Character>();
+        char[] inputsChars = set1.toCharArray();
+        for(char chars : inputsChars){
+            results.add(chars);
+        }
+
+        StringBuffer sb = new StringBuffer();
+        for(Character character : results){
+            sb.append(character);
+        }
+        return sb.toString();
+    }
+
+    public static boolean charactersAreUnique(String input) {
+        boolean[]matches = new boolean[256]; //ascii character set only
+        for(int i=0;i<input.length();i++){
+            Character pos = input.charAt(i);
+            if(matches[pos]){
+                return false;
+            }
+            matches[pos]=true;
+        }
+        return true;
+    }
+
+    public static boolean charactersAreUnique1(String input){
+        Set<Character>characters = new HashSet<Character>();
+        for(Character character : input.toCharArray()){
+            characters.add(character);
+        }
+        if(input.length()==characters.size()){
+            return true;
+        }else{
+            return false;
+        }
     }
 }
--- src/com/wesleyreisz/problem1/test/StringUtilTest.java
+++ src/com/wesleyreisz/problem1/test/StringUtilTest.java
@@ -20,4 +20,49 @@ public class StringUtilTest {
         Assert.assertEquals("sew", StringUtil.reverse("wes"));
         Assert.assertEquals("12345", StringUtil.reverse("54321"));
     }
+    @Test
+    public void testRemoveDups(){
+        String set1 = "wweess";
+        StringUtil.removeDuplicatesSolution(set1);
+        Assert.assertEquals("wes", set1);
+
+        String set2 = "111222234";
+        StringUtil.removeDuplicatesSolution(set2);
+        Assert.assertEquals("1234", set2);
+
+        String set3 = "42222242221";
+        StringUtil.removeDuplicatesSolution(set3);
+        Assert.assertEquals("4321", set3);
+    }
+
+    @Test
+    public void testRemoveDupsNew(){
+        String set1 = "wweess";
+        Assert.assertEquals("wes", StringUtil.removeDuplicatesSolutionNew(set1));
+
+        String set2 = "111222234";
+        Assert.assertEquals("3214", StringUtil.removeDuplicatesSolutionNew(set2));
+
+        String set3 = "42222242221";
+        Assert.assertEquals("214", StringUtil.removeDuplicatesSolutionNew(set3));
+
+        String set4 = "hhhhhh";
+        Assert.assertEquals("h", StringUtil.removeDuplicatesSolutionNew(set4));
+    }
+
+    @Test
+    public void testForUniqueKata(){
+        Assert.assertTrue(StringUtil.charactersAreUnique("wes"));
+        Assert.assertTrue(StringUtil.charactersAreUnique("ert"));
+        Assert.assertFalse(StringUtil.charactersAreUnique("tttt"));
+        Assert.assertFalse(StringUtil.charactersAreUnique("test"));
+    }
+
+    @Test
+    public void testForUniqueKata1(){
+        Assert.assertTrue(StringUtil.charactersAreUnique1("wes"));
+        Assert.assertTrue(StringUtil.charactersAreUnique1("ert"));
+        Assert.assertFalse(StringUtil.charactersAreUnique1("tttt"));
+        Assert.assertFalse(StringUtil.charactersAreUnique1("test"));
+    }
 }
